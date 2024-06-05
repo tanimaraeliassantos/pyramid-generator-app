fa395705c3c6bcdd87daa45bb9e0b7de275416c6
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-06-05 11:13:23 +0200

    Update script.js
    Adds comment for later task

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const rows = [];
 function padRow(rowNumber, rowCount) {
 	return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
-
+// Change code to a different kind of loop
 for (let i = 1; i <= count; i ++) {
 	rows.push(padRow(i, count));
 }
