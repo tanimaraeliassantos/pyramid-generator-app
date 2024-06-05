fc47cba8f06a6a5b18e3b3de1fba6365ff110f3b
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-06-05 11:14:05 +0200

    Update script.js
    Turns for loop into multi-line comment

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function padRow(rowNumber, rowCount) {
 	return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 // Change code to a different kind of loop
-for (let i = 1; i <= count; i ++) {
+/*for (let i = 1; i <= count; i ++) {
 	rows.push(padRow(i, count));
-}
+}*/
 
 let result = '';
 
