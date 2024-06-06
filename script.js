b5a1383ed127110b5abf228d4979d8589020f788
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-06-06 23:09:07 +0200

    Update script.js
    Creates another kind of loop

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount) {
-	return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+	return (
+		' '.repeat(rowCount - rowNumber) +
+		character.repeat(2 * rowNumber - 1) +
+		' '.repeat(rowCount - rowNumber)
+	);
 }
 // Change code to a different kind of loop
 /*for (let i = 1; i <= count; i ++) {
 	rows.push(padRow(i, count));
 }*/
+
+if (true) {
+	console.log('Condition is true');
+}
 
 let result = '';
 
