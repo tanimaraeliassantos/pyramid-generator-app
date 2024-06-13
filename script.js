c76f48fc702f5637e7ca5367200d74a12dd9b5ea
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-06-13 14:13:34 +0200

    Update script.js
    Set loop to run when condition is greater than 0

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function padRow(rowNumber, rowCount) {
 	rows.push(padRow(rows.length + 1, count));
 } */
 
-for (let i = count; false; false) {}
+for (let i = count; i > 0; false) {}
 let result = '';
 
 for (const row of rows) {
